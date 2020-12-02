aa67a21d0f4ddd0e2be8c7045e3144d296df5b4f
ryan <1184518756@qq.com>
2020-12-02 12:58:55 +0800

    update rollup.js's configuration & uploaded 0.0.7

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -10,17 +10,19 @@ const {
     main = 'dist/index.js',
     mainMini = 'dist/index.mini.js',
     module = 'dist/index.es.js',
+    moduleOrigin = 'dist/index.origin.esm.js',
     libName = '$func'
 } = pkg;
 
-const isESM = ['esm'].includes(NODE_ENV);
+const isESM = ['esm', 'esm5'].includes(NODE_ENV);
 const isProd = ['prod'].includes(NODE_ENV);
-const usingBabel = ['prod', 'normal'].includes(NODE_ENV);
+const usingBabel = ['prod', 'normal', 'esm5'].includes(NODE_ENV);
 
 const fileMapper = {
-    esm: module,
+    esm5: module,
     prod: mainMini,
-    normal: main
+    normal: main,
+    esm: moduleOrigin
 };
 
 export default {
