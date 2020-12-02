4a597dfd830a6ce6c61248401c11ee6572e839ac
ryan <1184518756@qq.com>
2020-12-02 12:48:25 +0800

    update to using rollup.js & uploaded 0.0.6

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-    presets: [['@babel/preset-env']],
+    "presets": [
+        [
+            "@babel/preset-env",
+            {
+                "modules": false
+            }
+        ]
+    ],
+    "plugins": [
+        ["@babel/plugin-transform-runtime"]
+    ],
+    "ignore": [
+        "node_modules/**"
+    ]
 };
